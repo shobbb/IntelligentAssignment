267bbfc3a772e4d761049427d7e444b73215ae51
Shoaib Akbar <shoaib429@gmail.com>
2016-06-17 12:13:47 -0400

    Deleted unnecessary file

--- app/main.py
+++
@@ -1,6 +0,0 @@
-import clustering as clst
-import top_trading_cycles as ttc
-
-exper_data = [[float(num) for num in lines] for lines in open("Auction.txt").read().splitlines()]
-teams,users = clst.kmeans_assignment(exper_data)
-ttc.team_swap(teams,users)
